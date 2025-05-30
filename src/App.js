942dc219a4b04ee8adb9ba7b7d7d23031cdd420c
saloni.t-at-178736008566 <saloni.thiyagarajan@exterro.com>
2025-05-30 22:57:20 +0530

    routing-changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header"
 import Body from "./components/Body"
 import { createBrowserRouter,Outlet,RouterProvider } from "react-router"
 import Contact from "./components/Contact"
-import About from "./components/about"
+import About from "./components/About"
 import Menu from "./components/Menu"
 import Error from "./components/Error"
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,9 @@
-import User from "./User"
 
 const About = () => {
   return (
     <div>
       <h1>about</h1>
-      <User name = {'saloni'}/>
+    
     </div>
   )
 }
