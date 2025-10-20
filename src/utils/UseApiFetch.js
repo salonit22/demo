649fbb380fb92a9fb39f7a61038343906fbf926d
saloni.t-at-178736008566 <saloni.thiyagarajan@exterro.com>
2025-10-20 22:37:03 +0530

    new changes

--- src/utils/UseApiFetch.js
+++ src/utils/UseApiFetch.js
@@ -7,7 +7,7 @@ const UseApiFetch = (id) => {
         fetchData()
     }, [])
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0361755&lng=77.0141536&restaurantId=" + id + "&submitAction=ENTER");
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0361755&lng=77.0141536&restaurantId=" + id + "&catalog_qa=undefined&submitAction=ENTER");
         const json = await data.json();
         setresInfo(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
